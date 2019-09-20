77dc69876180c29d56ab3af3da48e791a4235801
Programacion05 <Programacion05@tec-soft.local>
2019-09-20 17:00:53 +0200

    login primer commit

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          User History
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+
+        <q-item-label header> Menú Principal </q-item-label>
+
+        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
+
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label> Lista de Ofertas </q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+      
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MyLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,75 @@
+<template>
+
+    <div id="mainDiv" class="q-pa-md" style="max-width: 2500px">
+
+    <div class="column items-center">
+        <h1> Login </h1>
+    </div>
+
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-md column items-center"
+    >
+      <q-input
+        filled
+        v-model="username"
+        label="Usuario"
+        class="q-gutter-md"
+        style="width: 600px"
+      />
+
+      <q-input
+        filled
+        v-model="password"
+        label="Contraseña"
+        class="q-gutter-md"
+        style="width: 600px"
+      />
+
+      <div>
+        <q-btn label="Login" text-color="" type="submit" color="primary"  class="q-ml-md q-items-center q-pa-xs" style="width: 150px" />
+      </div>
+
+    </q-form>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'fas fa-exclamation-triangle',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'fas fa-check-circle',
+          message: 'Submitted'
+        })
+      }
+    },
+  }
+}
+</script>
+
+<style>
+   body {
+       background-color: #e1f5fe;
+    }
+</style>
